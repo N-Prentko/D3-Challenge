11aaecf09f885fd7c74e0dc77cead14e2c1e2ae5
Nicholas Prentkowski <nprentko@gmail.com>
2020-07-07 22:32:33 -0500

    updates

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,1 +1,38 @@
-// @TODO: YOUR CODE HERE!
+let svgWidth = 950;
+let svgHeight = 550;
+
+
+let margin = {
+    right: 40,
+    left: 100,
+    top: 30,
+    bottom: 85
+
+};
+
+let height = svgHeight - margin.top - margin.bottom;
+let width = svgWidth - margin.left - margin.right;
+
+var svg = d3.select("#scatter")
+    .append("svg")
+    .attr("height", svgHeight)
+    .attr("width", svgWidth);
+
+var chartGroup = svg.append("g")
+    .attr("transform", `translate(${margin.left}, ${margin.top})`);
+
+d3.csv("assets/data/data.csv").then(function(demoData, err) {
+
+    demoData.forEach(function(data) {
+        data.poverty =+ data.poverty;
+        data.healthcare =+ data.healthcare;
+    });
+
+    var xLinearScale = d3.scaleLinear()
+        .domain([d3.min(demoData, d => d.poverty) - 2, d3.max(demoData, d => d.poverty)])
+        .range([height, 0]);
+    
+    var yLinearScale = d3.scaleLinear()
+        .domain([0, d3.max(demoData, d => d.healthcare)])
+        .range([height, 0]);
+})
